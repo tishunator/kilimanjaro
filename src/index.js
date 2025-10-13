c2c4487df0149e9af6f9fa348aed55bcb15dec0c
Тиш <tishubezarius@mail.ru>
2025-10-13 17:35:42 +0300

    Добавила настройки слайдера для мобильной версии

--- src/index.js
+++ src/index.js
@@ -7,5 +7,12 @@ new Glide('.glide', {
   perView: 1,
   focusAt: 0,
   peek: { before: 0, after: 240 },
-  gap: 20
+  gap: 20,
+  breakpoints: {
+    768: {
+      perView: 1,
+      peek: { before: 0, after: 0 },
+      gap: 10
+    }
+  }
 }).mount();
